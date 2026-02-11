59256cf910d604444ffae9b65c8ce3c4f4f78027
Seif Kamal <seifkemos@gmail.com>
2026-02-11 08:20:58 +0200

    Adding BlogPost component

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,11 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GoArrowLeft, GoArrowRight } from "react-icons/go";
 import Viewall from "../icons/viewall.svg?react"
+import { domain } from '../store';
+import axios from 'axios';
 
 export default function BlogPost() {
+    const [blogs, setBlogs] = useState([])
     const [activeDot, setActiveDot] = useState(0)
+    const ITEMS_PER_PAGE = 5;
 
-    const dotsCount = 4;
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+    const dotsCount = Math.ceil(blogs.length / ITEMS_PER_PAGE);
+
+    useEffect(() => {
+        axios
+            .get(domain + "/api/blogs?populate=cover")
+            .then((res) => {
+                const data = res.data.data;
+                const sorted = data.sort((a, b) => {
+                    const orderA = a.order ?? 999;
+                    const orderB = b.order ?? 999;
+                    return orderA - orderB;
+                });
+
+                setBlogs(sorted);
+            })
+            .catch((err) => {
+                console.error("Failed to fetch Blogs:", err);
+            });
+    }, []);
+
+    const startIndex = activeDot * ITEMS_PER_PAGE;
+    const visibleBlogs = blogs.slice(
+        startIndex,
+        startIndex + ITEMS_PER_PAGE
+    );
+
+    useEffect(() => {
+        const onResize = () => {
+            const mobile = window.innerWidth < 768;
+            setIsMobile(mobile);
+
+            // 🔑 reset pagination when entering mobile
+            if (mobile) {
+                setActiveDot(0);
+            }
+        };
+
+        window.addEventListener("resize", onResize);
+        return () => window.removeEventListener("resize", onResize);
+    }, []);
+
     return (
         <div className="flex flex-col w-[382px] h-[303px] gap-[24px] md:w-[884px] md:h-[455px] lg:w-[1200px]">
             {/* Text Line */}
@@ -20,23 +65,51 @@ export default function BlogPost() {
 
                 <div className='hidden md:flex md:flex-col md:w-[96px] md:h-[51px] md:gap-[8px]'>
                     <div className='flex md:gap-[8px] md:w-[96px] md:h-[31px]'>
-                        <button onClick={() => { setActiveDot((prev) => (prev - 1 + dotsCount) % dotsCount) }} className="cursor-pointer flex justify-center items-center w-[44px] h-[32px] hover:scale-105 transition-[opacity,scale] border rounded-md text-[#ededed]">
+                        <button onClick={() => setActiveDot((prev) => Math.max(prev - 1, 0))} className="cursor-pointer flex justify-center items-center w-[44px] h-[32px] hover:scale-105 transition-[opacity,scale] border rounded-md text-[#ededed]">
                             <GoArrowLeft size={20} />
                         </button>
-                        <button onClick={() => setActiveDot((prev) => (prev + 1) % dotsCount)} className="cursor-pointer flex justify-center items-center w-[44px] h-[32px] hover:scale-105 transition-[opacity,scale] border rounded-md text-[#ededed]">
+                        <button onClick={() => setActiveDot((prev) => Math.min(prev + 1, dotsCount - 1))} className="cursor-pointer flex justify-center items-center w-[44px] h-[32px] hover:scale-105 transition-[opacity,scale] border rounded-md text-[#ededed]">
                             <GoArrowRight size={20} />
                         </button>
                     </div>
 
                     <div className='flex items-center w-full h-[12px] gap-[11px]'>
-                        {[0, 1, 2, 3].map((index) => (
+                        {Array.from({ length: dotsCount }).map((_, index) => (
                             <div key={index} onClick={() => { setActiveDot(index) }} className={`${activeDot === index ? "bg-[#FF5733] h-[12px] w-[28px] hover:bg-[#ff2f00] rounded-md cursor-pointer" : "bg-[#452154] hover:bg-[#ff2f00] w-[7px] h-[7px] hover:w-[28px] hover:h-[12px] rounded-md cursor-pointer"}`}></div>
                         ))}
                     </div>
                 </div>
 
-
             </div>
+            {/*////////// Blogs Section///////// */}
+            <div className='flex flex-nowrap no-scrollbar overflow-x-auto md:overflow-visible md:grid md:grid-cols-4 md:grid-rows-2 md:auto-rows-fr w-full md:max-w-[884px] h-[243px] gap-[12px] md:h-[380px] lg:max-w-[1200px]'>
+                {(isMobile ? blogs : visibleBlogs).map((el, index) => {
+                    const isBig = index === 0;
+
+                    return (
+                        <div
+                            key={el.id}
+                            className={`relative rounded-xl min-w-[280px] md:min-w-0 h-[243px] md:h-full bg-[#1C1B29] cursor-pointer overflow-hidden${isMobile ? "" : "transition-transform duration-300 ease-out hover:-translate-y-2"} ${isBig ? "md:col-span-2 md:row-span-2" : ""}`}
+                        >
+
+                            <img
+                                src={domain + el.cover?.url}
+                                alt={el.name}
+                                className="absolute rounded-xl inset-0 w-full h-[191px] md:h-full object-cover"
+                            />
+
+
+                            <div className="absolute rounded-xl inset-0 bg-black/30 h-[191px] md:h-full" />
+
+                            <h1 className={`absolute ${isMobile ? "bottom-0 text-[14px] text-center w-[275px]" : ` ${isBig ? "text-[20px] left-3 bottom-4 w-[574px]" : "text-[16px] left-2 bottom-1"}`} text-white `}>
+                                {el.description}
+                            </h1>
+                        </div>
+
+                    );
+                })}
+            </div>
+
         </div>
     )
 }
