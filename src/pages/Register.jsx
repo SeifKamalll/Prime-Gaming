09996bd423791ad7c664b1bbabbe4098c059b4de
Seif Kamal <seifkemos@gmail.com>
2026-02-18 06:38:58 +0200

    minor changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -39,21 +39,21 @@ export default function Register() {
 
     return (
 
-        <div data-theme="dark" className={`w-full h-dvh flex items-center justify-center`}>
+        <div data-theme="dark" className={`w-full h-dvh flex items-center justify-center bg-[#1C1B29] font-Vazirmatn`}>
             <Formik initialValues={{ email: '', password: '', username: '' }} onSubmit={handleSubmit} validationSchema={Schema}>
-                <Form className="shadow rounded-2xl p-4 flex flex-col gap-3 w-100 border">
+                <Form className="shadow rounded-2xl p-4 flex flex-col gap-3 w-100 border border-[#FF5733]">
                     <h1 className="text-2xl">Welcome Please Register</h1>
-                    <Field className="w-full input outline-0" name="username" type="text" placeholder="Enter your Username" />
+                    <Field className="w-full input outline-0 focus:border-[#FF5733]" name="username" type="text" placeholder="Enter your Username" />
                     <ErrorMessage name="username" component={'p'} className="text-red-500" />
 
-                    <Field className="w-full input outline-0" name="email" type="text" placeholder="Enter your Email" />
+                    <Field className="w-full input outline-0 focus:border-[#FF5733]" name="email" type="text" placeholder="Enter your Email" />
                     <ErrorMessage name="email" component={'p'} className="text-red-500" />
-                    <Field className="w-full input outline-0" name="password" type="text" placeholder="Enter your Password" />
+                    <Field className="w-full input outline-0 focus:border-[#FF5733]" name="password" type="text" placeholder="Enter your Password" />
                     <ErrorMessage name="password" component={'p'} className="text-red-500" />
-                    <button type="submit" className="btn btn-success w-full">
+                    <button type="submit" className="btn w-full bg-[#FF5733] hover:bg-gray-950 border-[#FF5733] text-white">
                         Register
                     </button>
-                    <Link to={'/login'} className="btn btn-primary">
+                    <Link to={'/login'} className="btn border-[#FF5733] text-[#FF5733] bg-transparent hover:bg-gray-950 hover:text-white">
                         Already have an Account ? Please Login
                     </Link>
                 </Form>
--- src/pages/loginpage.jsx
+++ src/pages/loginpage.jsx
@@ -34,22 +34,22 @@ export default function Loginpage() {
 
   return (
 
-    <div data-theme="dark" className="w-full h-dvh flex items-center justify-center">
+    <div data-theme="dark" className="w-full h-dvh flex items-center justify-center bg-[#1C1B29] font-Vazirmatn">
       <Formik initialValues={{ email: "", password: "", isChecked: false }} onSubmit={handleSubmit} validationSchema={Schema}>
-        <Form className="shadow rounded-2xl p-4 flex flex-col gap-3 w-[400px] border">
+        <Form className="shadow rounded-2xl p-4 flex flex-col gap-3 w-[400px] border border-[#FF5733]">
           <h1 className="text-2xl">Welcome , Please login</h1>
-          <Field name="email" type="text" className="w-full input outline-0" placeholder="Enter Your Email" />
+          <Field name="email" type="text" className="w-full input outline-0 focus:border-[#FF5733]" placeholder="Enter Your Email" />
           <ErrorMessage name="email" component={'p'} className="text-red-500" />
-          <Field name="password" type="text" className="w-full input outline-0" placeholder="Enter Your Password" />
+          <Field name="password" type="text" className="w-full input outline-0 focus:border-[#FF5733]" placeholder="Enter Your Password" />
           <ErrorMessage name="password" component={'p'} className="text-red-500" />
           <label className="flex gap-3 w-max">
-            <Field name="isChecked" className="checkbox checkbox-primary cursor-pointer" type="checkbox" />
+            <Field name="isChecked" className="checkbox border-[#FF5733] cursor-pointer" type="checkbox" />
             Remember Me
           </label>
-          <button type="submit" className="btn btn-primary w-full">
+          <button type="submit" className="btn w-full border-[#FF5733] text-[#FF5733] bg-transparent hover:bg-gray-950 hover:text-white">
             Login
           </button>
-          <Link to={'/register'} className="btn btn-success">
+          <Link to={'/register'} className="btn bg-[#FF5733] hover:bg-gray-950 border-[#FF5733] text-white">
             Don't have an Account ? Register now
           </Link>
         </Form>
